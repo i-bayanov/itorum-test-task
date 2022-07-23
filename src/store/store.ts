1ed1ccec007cce262d353a1bd6a2c1ad4c23a9ca
Ilya Bayanov <i.p.bayanov@gmail.com>
2022-07-23 18:23:44 +0300

    update store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 
+import { githubApi } from './githubApi';
+import { searchQueryReducer } from './search_query_reducer';
+
 export const store = configureStore({
   reducer: {
+    [githubApi.reducerPath]: githubApi.reducer,
+    search: searchQueryReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(githubApi.middleware),
 });
 
 export type AppDispatch = typeof store.dispatch;
