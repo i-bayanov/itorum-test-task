e8951263446e3465e52f9c85f8f2a57cb986bb9f
Ilya Bayanov <i.p.bayanov@gmail.com>
2022-07-23 18:24:27 +0300

    create hook for operating with search query string

--- src/store/hooks.ts
+++ src/store/hooks.ts
@@ -1,6 +1,15 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import { bindActionCreators } from '@reduxjs/toolkit';
+
 import type { RootState, AppDispatch } from './store';
+import { searchQueryActions } from './search_query_reducer';
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export const useAction = () => {
+  const dispatch = useAppDispatch();
+
+  return bindActionCreators(searchQueryActions, dispatch);
+};
