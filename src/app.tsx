9cd16cab54244c0042ee9b28090712db60bde7db
Ilya Bayanov <i.p.bayanov@gmail.com>
2022-07-23 18:20:10 +0300

    update app

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,11 @@
+import SearchField from "./components/search_field/search_field";
+import UserList from "./components/user_list/user_list";
+
 export default function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <SearchField />
+      <UserList />
+    </div>
   );
 }
