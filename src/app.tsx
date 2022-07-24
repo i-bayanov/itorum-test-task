e70fd45f0e6517492f388e3f4c3bcc5c6479f1bd
Ilya Bayanov <i.p.bayanov@gmail.com>
2022-07-24 19:57:46 +0300

    add routing

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,23 @@
+import { Route, Routes } from "react-router-dom";
+
 import SearchField from "./components/search_field/search_field";
 import UserList from "./components/user_list/user_list";
+import UserPage from "./components/user_page/user_page";
 
 export default function App() {
   return (
     <div className="App">
-      <SearchField />
-      <UserList />
+      <Routes>
+        <Route path="/" element={<>
+          <SearchField />
+          <UserList />
+        </>} />
+        <Route path="/page:num" element={<>
+          <SearchField />
+          <UserList />
+        </>} />
+        <Route path="/:login" element={<UserPage />} />
+      </Routes>
     </div>
   );
 }
