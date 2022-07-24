2294e311b0e876488799ea28560a4bb1d5a77714
Ilya Bayanov <i.p.bayanov@gmail.com>
2022-07-24 20:16:01 +0300

    add page reducer to store

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import { githubApi } from './githubApi';
 import { searchQueryReducer } from './search_query_reducer';
+import { pageReducer } from './page_reducer';
 
 export const store = configureStore({
   reducer: {
     [githubApi.reducerPath]: githubApi.reducer,
     search: searchQueryReducer,
+    page: pageReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(githubApi.middleware),
 });
