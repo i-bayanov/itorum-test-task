b7209078445ab2e2e9f2f840cbef63f6be1896f7
Ilya Bayanov <i.p.bayanov@gmail.com>
2022-07-24 20:13:45 +0300

    complete user list

--- src/components/user_list/user_list.tsx
+++ src/components/user_list/user_list.tsx
@@ -1,21 +1,56 @@
+import { useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
+
 import { useSearchQuery } from '../../store/githubApi';
-import { useAppSelector } from '../../store/hooks';
+import { useAppSelector, usePageAction } from '../../store/hooks';
+
+import UserCard from '../user_card/user_card';
 
 import './user_list.css';
 
 export default function UserList() {
+  const { num } = useParams();
+  const pageNum = Number(num) || 1;
   const { query } = useAppSelector(state => state.search);
-  const {isLoading, isError, data} = useSearchQuery(query, {
-    skip: query.length < 3,
-  });
+  const {isFetching, isError, data} = useSearchQuery(
+    {q: query, page: pageNum},
+    { skip: query.length < 3 },
+  );
+  const pagesTotal = Math.ceil(data?.total_count! / 9);
+  const { updatePage } = usePageAction();
+
+  useEffect(() => { updatePage(pageNum); });
 
   return (
     <div className='user-list'>
-      {isLoading && <p>Loading...</p>}
+      {isFetching && <p>Loading...</p>}
       {isError && <p>Something went wrong!</p>}
-      <ul>
-        {data?.items.map((item) => <li key={item.id}>{item.login}</li>)}
-      </ul>
+      {query.length >=3
+        && !isFetching
+        && !isError
+        && Boolean(data?.items.length)
+        && <>
+          <div className='list-wrapper'>
+            {data?.items.map((item) => <UserCard key={item.id} {...item}/>)}
+          </div>
+          <div className='prev-next'>
+            <Link
+              to={`/page${pageNum - 1}`}
+              className={pageNum === 1 ? 'disabled-link' : ''}
+            >&lt; PREV.</Link>
+            <Link
+              to={`/page${pageNum + 1}`}
+              className={pageNum === pagesTotal ? 'disabled-link' : ''}
+            >NEXT &gt;</Link>
+          </div>
+        </>
+      }
+      {query.length >= 3
+        && !isFetching
+        && !isError
+        && !data?.items.length
+        && <div>Nothing found</div>
+      }
     </div>
   );
 }
